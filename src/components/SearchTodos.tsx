7ade13ec4d64ab1c4542b01f6f273913eb0f5e4c
Somai Dhia <dhiasomai@gmail.com>
2026-04-05 20:28:52 +0200

    add check uncheck all todos

--- src/components/SearchTodos.tsx
+++ src/components/SearchTodos.tsx
@@ -2,11 +2,22 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useState} from 'react';
 
 
-export default function SearchTodos() {
+export default function SearchTodos({setCheckedTodos}:any) {
+  const [applychecked, setApplyChecked] = useState(false);
   return (<>
-    <Button variant="outlined" sx={{display: 'block',margin:'auto',marginTop:2}}>apply the research</Button>
+    <Button variant="outlined" sx={{display: 'block',margin:'auto',marginTop:2}} onClick={()=>{
+      if (!applychecked){
+      setCheckedTodos((prevCheckedTodos: boolean[]) => prevCheckedTodos.map(() => true))}
+      
+      else {
+        setCheckedTodos((prevCheckedTodos: boolean[]) => prevCheckedTodos.map(() => false))
+      };
+      setApplyChecked((prevApplyChecked: boolean) => !prevApplyChecked);
+      
+    }}>{applychecked ? 'uncheck all' : 'check all'}</Button>
      <Box sx={{ width: 500, maxWidth: '100%',margin:'auto',marginTop:2 }}>
       <TextField fullWidth label="search in the Todos" id="fullWidth" />
     </Box></>)}
